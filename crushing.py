e205a9d0b975925379c4149f7c7d44196a58090d
SirPecos <buzur_pavel@mail.ru>
2024-01-17 19:22:46 +0300

    Changes made

--- crushing.py
+++ crushing.py
@@ -10,17 +10,43 @@ class Crushing:
         self.screen.fill((0, 0, 0))
         self.current = None
         self.start_sprites = pygame.sprite.Group()
-        self.end_sprites = pygame.sprite.Group()
+        self.end_1_sprites = pygame.sprite.Group()
+        self.end_2_sprites = pygame.sprite.Group()
         self.first_fill = False
 
-        iron_image = load_image('iron_ingots.png', -1)
-        iron = pygame.sprite.Sprite(self.start_sprites)
-        iron_image = pygame.transform.scale(iron_image, (280, 200))
-        self.iron_id = id(iron)
-        iron.image = iron_image
-        iron.rect = iron.image.get_rect()
-        iron.rect.x = 150
-        iron.rect.y = 200
+        f1 = pygame.font.Font(None, 72)
+        self.text1 = f1.render('iron', True, (255, 255, 255))
+
+        f2 = pygame.font.Font(None, 72)
+        self.text2 = f2.render('iron nuggets', True, (255, 255, 255))
+
+        f3 = pygame.font.Font(None, 72)
+        self.text3 = f3.render('0', True, (255, 255, 255))
+
+        f4 = pygame.font.Font(None, 72)
+        self.text4 = f4.render('0', True, (255, 255, 255))
+
+        f5 = pygame.font.Font(None, 72)
+        self.text5 = f5.render('gold', True, (255, 255, 255))
+
+        f6 = pygame.font.Font(None, 72)
+        self.text6 = f6.render('gold nuggets', True, (255, 255, 255))
+
+        f7 = pygame.font.Font(None, 72)
+        self.text7 = f7.render('0', True, (255, 255, 255))
+
+        f8 = pygame.font.Font(None, 72)
+        self.text8 = f8.render('0', True, (255, 255, 255))
+
+
+        iron_ingots_image = load_image('iron_ingots.png', -1)
+        iron_ingots = pygame.sprite.Sprite(self.start_sprites)
+        iron_ingots_image = pygame.transform.scale(iron_ingots_image, (280, 200))
+        self.iron_id = id(iron_ingots)
+        iron_ingots.image = iron_ingots_image
+        iron_ingots.rect = iron_ingots.image.get_rect()
+        iron_ingots.rect.x = 150
+        iron_ingots.rect.y = 400
 
         # iron_image_highlighted = load_image('iron_ingots_highlited.png', -1)
         # iron_highlighted = pygame.sprite.Sprite()
@@ -31,14 +57,14 @@ class Crushing:
         # iron_highlighted.rect.x = 150
         # iron_highlighted.rect.y = 200
 
-        gold_image = load_image('gold_ingots.png', -1)
-        gold = pygame.sprite.Sprite(self.start_sprites)
-        gold_image = pygame.transform.scale(gold_image, (280, 200))
-        self.gold_id = id(gold)
-        gold.image = gold_image
-        gold.rect = gold.image.get_rect()
-        gold.rect.x = 570
-        gold.rect.y = 200
+        gold_ingots_image = load_image('gold_ingots.png', -1)
+        gold_ingots = pygame.sprite.Sprite(self.start_sprites)
+        gold_ingots_image = pygame.transform.scale(gold_ingots_image, (280, 200))
+        self.gold_id = id(gold_ingots)
+        gold_ingots.image = gold_ingots_image
+        gold_ingots.rect = gold_ingots.image.get_rect()
+        gold_ingots.rect.x = 570
+        gold_ingots.rect.y = 400
 
         # gold_image_highlighted = load_image('gold_ingots_highlited.png', -1)
         # gold_highlighted = pygame.sprite.Sprite()
@@ -50,12 +76,36 @@ class Crushing:
         # gold_highlighted.rect.y = 200
 
         metal_sheet_image = load_image('metal_sheet.png', -1)
-        metal_sheet = pygame.sprite.Sprite(self.end_sprites)
+        metal_sheet = pygame.sprite.Sprite(self.end_1_sprites, self.end_2_sprites)
         metal_sheet_image = pygame.transform.scale(metal_sheet_image, (200, 200))
         metal_sheet.image = metal_sheet_image
         metal_sheet.rect = metal_sheet.image.get_rect()
         metal_sheet.rect.x = 400
         metal_sheet.rect.y = 400
+
+        iron_image = load_image('iron.png', -1)
+        iron = pygame.sprite.Sprite(self.end_1_sprites)
+        iron_image = pygame.transform.scale(iron_image, (200, 200))
+        iron.image = iron_image
+        iron.rect = iron.image.get_rect()
+        iron.rect.x = 400
+        iron.rect.y = 400
+
+        gold_image = load_image('gold.png', -1)
+        gold = pygame.sprite.Sprite(self.end_2_sprites)
+        gold_image = pygame.transform.scale(gold_image, (200, 200))
+        gold.image = gold_image
+        gold.rect = gold.image.get_rect()
+        gold.rect.x = 400
+        gold.rect.y = 400
+
+        crush_image = load_image('crush.png', -1)
+        crush = pygame.sprite.Sprite(self.end_1_sprites, self.end_2_sprites)
+        crush_image = pygame.transform.scale(crush_image, (250, 250))
+        crush.image = crush_image
+        crush.rect = crush.image.get_rect()
+        crush.rect.x = 360
+        crush.rect.y = 80
 
         self.start_sprites.draw(self.screen)
         pygame.display.flip()
@@ -84,28 +134,40 @@ class Crushing:
             elif self.current == 1:
                 if not self.first_fill:
                     self.screen.fill((0, 0, 0))
-                    self.end_sprites.draw(self.screen)
-                    pygame.display.flip()
+                    self.end_1_sprites.draw(self.screen)
                     self.first_fill = True
+                    self.screen.blit(self.text1, (50, 800))
+                    self.screen.blit(self.text2, (650, 800))
+                    self.screen.blit(self.text3, (50, 870))
+                    self.screen.blit(self.text4, (650, 870))
+                    pygame.display.flip()
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
                             self.current = None
+                            self.screen.fill((0, 0, 0))
                             self.start_sprites.draw(self.screen)
                             pygame.display.flip()
                             self.first_fill = False
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        if
 
 
             elif self.current == 2:
                 if not self.first_fill:
                     self.screen.fill((0, 0, 0))
-                    self.end_sprites.draw(self.screen)
+                    self.end_2_sprites.draw(self.screen)
+                    self.screen.blit(self.text5, (50, 800))
+                    self.screen.blit(self.text6, (650, 800))
+                    self.screen.blit(self.text7, (50, 870))
+                    self.screen.blit(self.text8, (650, 870))
                     pygame.display.flip()
                     self.first_fill = True
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
                             self.current = None
+                            self.screen.fill((0, 0, 0))
                             self.start_sprites.draw(self.screen)
                             pygame.display.flip()
                             self.first_fill = False
